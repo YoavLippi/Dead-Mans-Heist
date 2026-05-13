58750f5bd00ca5faee27b21b5cf226b670ce3481
gingeninge <thomassheldon539@gmail.com>
2026-05-13 22:27:47 +0200

    Setup

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+enum EnemyStates 
+{
+    Patrolling,
+    Chasing,
+    Idle,
+    Stunned,
+    Recovery
+}
+public class Enemy : MonoBehaviour
+{
+  
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
