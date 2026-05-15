dd140af270ccdc01fecc09677e1a3208eb1de0d3
gingeninge <thomassheldon539@gmail.com>
2026-05-15 13:51:40 +0200

    worldstuff

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,16 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+{  
+     public NavMeshAgent nav;
+
+    [System.Serializable]
+    public struct worldEvents 
     {
-        
+        public int timeTrigger;
+        public Transform checkPointLocation;
+    }
+    public int index;
+    public List<worldEvents> schedules;
+
+    void OnEnable()
+    {
+        WorldTime.secondsChange += checkTime;
+    }
+  void OnDisable()
+    {
+        WorldTime.secondsChange -= checkTime;
+    }
+ 
+    public void checkTime(int currentworld) 
+    {
+        if (index > schedules.Count) return;
+        worldEvents next = schedules[index];
+        if (currentworld >= next.timeTrigger) 
+        {
+            moveToCheckPoint(next.checkPointLocation);
+            index++;
+        }
+
+            
+         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void moveToCheckPoint(Transform target) 
     {
-        
+       if(target !=null && nav != null) 
+        {
+            nav.SetDestination(target.position);
+        }
     }
 }
---
+++ Assets/Scripts/WorldTime.cs
@@ -0,0 +1,42 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class WorldTime : MonoBehaviour
+{
+    public static event Action<int> secondsChange;
+
+    [SerializeField] private float seconds = 1f;
+    private int currentSecond = 0;
+    private float timer = 0f;
+    [SerializeField]private TextMeshProUGUI text;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= seconds) 
+        {
+            timer -= seconds;
+            convertSeconds();
+            currentSecond++;
+            secondsChange?.Invoke(currentSecond);
+        }
+    }
+
+    private void convertSeconds() 
+    {
+        if (text != null) {
+        TimeSpan time = TimeSpan.FromSeconds(currentSecond);
+        text.text = time.ToString("mm':'ss");
+
+
+    } }
+}
