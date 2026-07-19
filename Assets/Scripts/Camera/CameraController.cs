e677936331bd0bbe18b8d41d5858e4befce5db16
YoavLippi <2567169@students.wits.ac.za>
2026-07-19 21:43:10 +0200

    Small changes

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour
     public static CameraController Instance;
     [SerializeField] private CinemachineBrain camBrain;
     [SerializeField] private ICinemachineCamera activeCam;
+    [SerializeField] private GameObject ghostVisionPanel;
 
     public CinemachineBrain CamBrain => camBrain;
 
@@ -28,16 +29,22 @@ public class CameraController : MonoBehaviour
     private void Start()
     {
         camBrain = GetComponent<CinemachineBrain>();
-        activeCam = camBrain.ActiveVirtualCamera;
+        //activeCam = camBrain.ActiveVirtualCamera;
+        if (camBrain.ActiveVirtualCamera is CinemachineCamera cam)
+        {
+            SetActiveCamera(cam);
+        }
     }
 
     public void SetActiveCamera(CinemachineCamera newActive)
     {
-        if (activeCam is CinemachineCamera cam)
+        if (activeCam != null && activeCam is CinemachineCamera cam)
         {
             cam.Priority = 10;
         }
         newActive.Priority = 20;
+        ghostVisionPanel.transform.position = (newActive.transform.position + newActive.transform.forward);
+        ghostVisionPanel.transform.rotation = newActive.transform.rotation;
         activeCam = newActive;
     }
 }
